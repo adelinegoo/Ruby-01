5bcb51656a5d993bc831098d1c7f323573da329d
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-10 18:49:08 +0100

    Add files via upload

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,23 @@
+def half_pyramid
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages tu veux ? "
+n = (gets.chomp).to_i
+m = 0
+n.times do
+  n -=1
+  m +=1
+  puts (" " * n) + ("#" * m)
+
+end
+
+end
+
+def full_pyramid(half_pyramid)
+puts #half_pyramid + ("#" * (m-1)) + (" " * (n-1))
+
+end
+
+def perform
+  n = half_pyramid
+  full_pyramid(half_pyramid)
+end
+perform
