ab3ee17f588cf08663dad80b31a0f56894d5ea58
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-10 17:34:51 +0100

    Update 00_hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,8 +1,20 @@
 #!ask ask_first_name 
 
-puts "Quel est ton prenom?"
-first_name = gets.chomp
-
-def h(first_name)
-  puts "Bonjour #{first_name} !"
+#!ask ask_first_name 
+def ask_first_name
+  puts "Quel est ton prénom ?"
+  print "> "
+  first_name = gets.chomp
+  return fist_name
 end
+
+def say_hello(first_name)
+  puts "Bonjour, #{first_name}!"
+end
+ 
+def perform
+  first_name = ask_first_name
+  say_hello(first_name)
+end
+
+perform
