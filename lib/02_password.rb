62c90f38007e2a3019e623c072d5cba3b918067c
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-10 20:41:03 +0100

    Update 02_password.rb

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,28 +1,38 @@
-
+#!demandant à l'utilisateur de définir un mot de passe
 def ask_signup
   puts " Merci d'entrer votre mot de passe."
   print "> "
   signup = gets.chomp
-  return password
+return
 end
 
+
 #!demandant à l'utilisateur de rentrer son mot de passe jusqu'à ce qu'il corresponde à celui défini précédemment
-def login(ask_signup)
+def ask_login(asignup)
+  login = ask_signup
+  login = nil
   puts "Merci d'ecrire votre mot de passe."
   print "> "
-  login = gets.chomp
-  login = signup
-  if login = signup
+  system 'stty -echo'
+  login = $stdin.gets.chomp
+    system 'stty -echo'
+   
+  if login == signup
     puts "youpi"
-  else 
-    puts ask_signup
+  
+  else
+    puts "Mot de passe errone"
+    
   end
 end
 
-def perform
-  signup = ask_signup
-  login(ask_signup)
-  
+
 #!affichant un écran de bienvenue avec des informations top secrètes de la NSA
 def welcome_screen
+  puts "Bienvenue dans la zone secrete! Voici les sms de Margaux"
 end
+
+def perform
+end
+
+perform
