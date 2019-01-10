2c055b368665a452896b8c002694068ab39be588
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-10 22:01:53 +0100

    Add files via upload

---
+++ lib/03_stairway.rb
@@ -0,0 +1,26 @@
+#!Stairway
+def dice
+	dice = rend(1..6)
+	return
+	end
+
+def game
+#!S’il 5 ou 6, il avance 1 marche et le programme le lui dit 
+  if dice == 5 || dice == 6
+	   marche += 1
+	    puts "tu montes d'#{dice} marche."
+	
+#! S’il fait 1, il descend d'une marche et le programme le lui dit 
+  elseif 
+	   dice == 1
+	   marche -= 1
+	   puts "tu descends d'#{dice} marche."
+	
+
+#!S’il fait 2, 3, ou 4 rien ne se passe, et le programme le lui dit 
+  else 
+	   puts "#{dice} rien ne se passe."
+	
+   end
+   return marche
+end 
