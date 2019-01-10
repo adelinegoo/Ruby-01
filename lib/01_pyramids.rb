30fdee1cd748502f8f03c3632f1e3de3fe718a0b
adelinegoo <46303718+adelinegoo@users.noreply.github.com>
2019-01-10 18:54:37 +0100

    Update 01_pyramids.rb

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,23 +1,22 @@
 def half_pyramid
-puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages tu veux ? "
-n = (gets.chomp).to_i
-m = 0
-n.times do
-  n -=1
-  m +=1
-  puts (" " * n) + ("#" * m)
-
-end
-
+  puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages tu veux ? "
+  n = (gets.chomp).to_i
+  m = 0
+    
+  n.times do
+      n -=1
+      m +=1
+      puts (" " * n) + ("#" * m)
+    end
 end
 
 def full_pyramid(half_pyramid)
-puts #half_pyramid + ("#" * (m-1)) + (" " * (n-1))
-
+  puts #half_pyramid + ("#" * (m-1)) + (" " * (n-1))
 end
 
 def perform
   n = half_pyramid
   full_pyramid(half_pyramid)
 end
+
 perform
